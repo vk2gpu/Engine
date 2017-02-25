d2c0573b40fbcb52cbb46d80c16935a1bf8b9787
Neil Forbes-Richardson <neilo@neilo.gd>
2017-02-25 21:34:38 +1100

    Added copyright notice for MPMCBoundedQueue, fixed up a couple of the errors I made.

--- src/core/mpmc_bounded_queue.h
+++ src/core/mpmc_bounded_queue.h
@@ -5,8 +5,22 @@
 
 namespace Core
 {
-	/**
-	 * MPMC Queue, by Dmitry Vyukov.
+	/*  Multi-producer/multi-consumer bounded queue.
+	 *  Copyright (c) 2010-2011, Dmitry Vyukov. All rights reserved.
+	 *  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+	 *     1. Redistributions of source code must retain the above copyright notice, this list of
+	 *        conditions and the following disclaimer.
+	 *     2. Redistributions in binary form must reproduce the above copyright notice, this list
+	 *        of conditions and the following disclaimer in the documentation and/or other materials
+	 *        provided with the distribution.
+	 *  THIS SOFTWARE IS PROVIDED BY DMITRY VYUKOV "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+	 *  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+	 *  DMITRY VYUKOV OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+	 *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+	 *  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+	 *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+	 *  The views and conclusions contained in the software and documentation are those of the authors and should not be interpreted
+	 *  as representing official policies, either expressed or implied, of Dmitry Vyukov.
 	 * http://www.1024cores.net/home/lock-free-algorithms/queues/bounded-mpmc-queue
 	 */
 	template<typename TYPE>
@@ -19,10 +33,11 @@ namespace Core
 		    , bufferMask_(size - 1)
 		{
 			DBG_ASSERT((size >= 2) && ((size & (size - 1)) == 0));
+			Core::Barrier();
 			for(i32 i = 0; i != size; i += 1)
-				Core::AtomicExchg(&buffer_[i].sequence_, i);
-			Core::AtomicExchg(&enqueuePos_, 0);
-			Core::AtomicExchg(&dequeuePos_, 0);
+				buffer_[i].sequence_ = i;
+			enqueuePos_ = 0;
+			dequeuePos_ = 0;
 		}
 		MPMCBoundedQueue(MPMCBoundedQueue&& other)
 		{
@@ -58,7 +73,7 @@ namespace Core
 				i32 dif = seq - pos;
 				if(dif == 0)
 				{
-					if(Core::AtomicCmpExchg(&enqueuePos_, pos + 1, pos) == pos) // was compare_exchange_weak
+					if(Core::AtomicCmpExchg(&enqueuePos_, pos + 1, pos) == pos)
 						break;
 				}
 				else if (dif < 0)
@@ -67,7 +82,8 @@ namespace Core
 					pos = Core::AtomicCmpExchg(&enqueuePos_, 0, 0);
 			}
 			cell->data_ = data;
-			Core::AtomicExchg(&cell->sequence_, pos + 1); // was release
+			Core::Barrier();
+			cell->sequence_ = pos + 1;
 			return true;
 		}
 
@@ -82,7 +98,7 @@ namespace Core
 				i32 dif = seq - (pos + 1);
 				if(dif == 0)
 				{
-					if(Core::AtomicCmpExchg(&dequeuePos_, pos + 1, pos) == pos) // was compare_exchange_weak
+					if(Core::AtomicCmpExchg(&dequeuePos_, pos + 1, pos) == pos)
 						break;
 				}
 				else if (dif < 0)
@@ -91,7 +107,8 @@ namespace Core
 					pos = Core::AtomicCmpExchg(&dequeuePos_, 0, 0);
 			}
 			data = cell->data_;
-			Core::AtomicExchg(&cell->sequence_, pos + bufferMask_ + 1); // was release
+			Core::Barrier();
+			cell->sequence_ = pos + bufferMask_ + 1;
 			return true;
 		}
 
