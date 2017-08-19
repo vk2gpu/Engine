41623a8308951ed28c4494da5a991903d948622b
Neil Forbes-Richardson <neilo@neilo.gd>
2017-08-19 19:35:00 +1000

    Added Core::Function, equiv to std::function but with fixed size.

---
+++ src/core/function.h
@@ -0,0 +1,123 @@
+#pragma once
+
+#include "core/debug.h"
+
+#include <utility>
+
+namespace Core
+{
+	namespace Detail
+	{
+		template<typename SIGNATURE>
+		class FunctionObjectBase;
+
+		template<typename RET, typename... ARGS>
+		class FunctionObjectBase<RET(ARGS...)>
+		{
+		public:
+			using func_obj_base = Detail::FunctionObjectBase<RET(ARGS...)>;
+
+			FunctionObjectBase() {}
+			virtual ~FunctionObjectBase() {}
+
+			virtual func_obj_base* DoCopy(void* storage) const = 0;
+			virtual RET DoCall(ARGS&&... args) const = 0;
+		};
+
+		template<typename CALLABLE, typename SIGNATURE>
+		class FunctionObject;
+
+		template<typename CALLABLE, typename RET, typename... ARGS>
+		class FunctionObject<CALLABLE, RET(ARGS...)> : public FunctionObjectBase<RET(ARGS...)>
+		{
+		public:
+			using func_obj_base = Detail::FunctionObjectBase<RET(ARGS...)>;
+			using func_obj = Detail::FunctionObject<CALLABLE, RET(ARGS...)>;
+
+			FunctionObject(const CALLABLE&& callable)
+			    : callable_(std::forward<const CALLABLE>(callable))
+			{
+			}
+
+			virtual ~FunctionObject() {}
+
+			func_obj_base* DoCopy(void* storage) const override
+			{
+				return new(storage) func_obj(std::forward<const CALLABLE>(callable_));
+			}
+			RET DoCall(ARGS&&... args) const override { return callable_(std::forward<ARGS>(args)...); }
+
+			CALLABLE callable_;
+		};
+	}
+
+	template<typename SIGNATURE, i32 MAX_SIZE = 32>
+	class Function;
+
+	template<typename RET, typename... ARGS, i32 MAX_SIZE>
+	class Function<RET(ARGS...), MAX_SIZE>;
+
+	template<typename SIGNATURE>
+	struct IsFunction
+	{
+		static constexpr bool value = false;
+	};
+
+	template<typename RET, typename... ARGS, i32 MAX_SIZE>
+	struct IsFunction<Function<RET(ARGS...), MAX_SIZE>>
+	{
+		static constexpr bool value = true;
+	};
+
+	template<typename RET, typename... ARGS, i32 MAX_SIZE>
+	class Function<RET(ARGS...), MAX_SIZE>
+	{
+	public:
+		using func_obj_base = Detail::FunctionObjectBase<RET(ARGS...)>;
+
+		template<typename CALLABLE>
+		using func_obj = Detail::FunctionObject<CALLABLE, RET(ARGS...)>;
+
+		Function() {}
+		Function(nullptr_t) {}
+
+		Function(const Function& func)
+		{
+			if(func.funcObj_)
+				funcObj_ = func.funcObj_->DoCopy(storage_);
+		}
+
+		Function& operator = (const Function& func)
+		{
+			if(func.funcObj_)
+				funcObj_ = func.funcObj_->DoCopy(storage_);
+			return *this;
+		}
+
+		template<typename CALLABLE, typename = typename std::enable_if_t<!IsFunction<CALLABLE>::value>>
+		Function(CALLABLE callable)
+		{
+			static_assert(sizeof(func_obj<CALLABLE>) <= MAX_SIZE, "MAX_SIZE too small for wrapped callable.");
+			funcObj_ = new(&storage_[0]) func_obj<CALLABLE>(std::move(callable));
+		}
+
+		~Function()
+		{
+			if(funcObj_)
+			{
+				funcObj_->~func_obj_base();
+				funcObj_ = nullptr;
+			}
+		}
+		RET operator()(ARGS&&... args) const
+		{
+			DBG_ASSERT(funcObj_);
+			return funcObj_->DoCall(std::forward<ARGS>(args)...);
+		}
+		operator bool() { return !!funcObj_; }
+
+	private:
+		u8 storage_[MAX_SIZE];
+		func_obj_base* funcObj_ = nullptr;
+	};
+} // namespace Core
---
+++ src/core/tests/function_tests.cpp
@@ -0,0 +1,129 @@
+#include "core/debug.h"
+#include "core/function.h"
+#include "core/vector.h"
+
+#include "catch.hpp"
+
+namespace
+{
+	struct AllocatorTest : public Core::Allocator
+	{
+		static i32 numBytes_;
+
+		void* allocate(index_type count, index_type size)
+		{
+			numBytes_ += count * size;
+			return Core::Allocator::allocate(count, size);
+		}
+
+		void deallocate(void* mem, index_type count, index_type size)
+		{
+			numBytes_ -= count * size;
+			Core::Allocator::deallocate(mem, count, size);
+		}
+	};
+
+	i32 AllocatorTest::numBytes_ = 0;
+}
+
+TEST_CASE("function-tests-basic")
+{
+	auto lambda = [](i32) {};
+
+	Core::Function<void(i32)> func;
+	REQUIRE(func == false);
+	func = lambda;
+	REQUIRE(func == true);
+}
+
+TEST_CASE("function-tests-copy")
+{
+	SECTION("copy")
+	{
+		auto lambda = [](i32) {};
+		Core::Function<void(i32)> func;
+		REQUIRE(func == false);
+		func = lambda;
+		REQUIRE(func == true);
+
+		Core::Function<void(i32)> func2 = func;
+		REQUIRE(func == true);
+		REQUIRE(func2 == true);
+	}
+
+	SECTION("copy-capture")
+	{
+		i32 a = 123;
+		auto lambda = [a](i32 b) { return a * b; };
+		REQUIRE(lambda(2) == (a*2));
+		Core::Function<i32(i32)> func;
+		REQUIRE(func == false);
+		func = lambda;
+		REQUIRE(func == true);
+		REQUIRE(func(2) == (a*2));
+
+		Core::Function<i32(i32)> func2 = func;
+		REQUIRE(func == true);
+		REQUIRE(func(2) == (a*2));
+		REQUIRE(func2 == true);
+		REQUIRE(func2(2) == (a*2));
+	}
+}
+
+TEST_CASE("function-tests-alloc")
+{
+	using Vector = Core::Vector<i32, AllocatorTest>;
+	Core::Function<i32(i32)> func;
+
+	Vector vec;
+	for(i32 idx = 0; idx < 32; ++idx)
+	{
+		vec.push_back(idx);
+	}
+
+	i32 size0 = AllocatorTest::numBytes_;
+
+	func = [vec](i32 idx){ return vec[idx]; };
+
+	i32 size1 = AllocatorTest::numBytes_;
+	REQUIRE(size1 > size0);
+
+	for(i32 idx = 0; idx < 32; ++idx)
+	{
+		REQUIRE(func(idx) == vec[idx]);
+		REQUIRE(func(idx) == vec[idx]);
+	}
+
+	std::function
+	vec.resize(0);
+
+	i32 size2 = AllocatorTest::numBytes_;
+	REQUIRE(size2 == size0);
+}
+
+
+TEST_CASE("function-tests-vector")
+{
+	Core::Vector<Core::Function<i32()>> funcs;
+
+	for(i32 idx = 0; idx < 32; ++idx)
+	{
+		funcs.push_back([idx](){ return idx * 2; });
+	}
+
+	for(i32 idx = 0; idx < 32; ++idx)
+	{
+		const auto& func = funcs[idx];
+		REQUIRE(func() == (idx * 2));
+	}
+
+	Core::Vector<Core::Function<i32()>> funcs2 = funcs;
+
+	for(i32 idx = 0; idx < 32; ++idx)
+	{
+		const auto& func = funcs[idx];
+		const auto& func2 = funcs2[idx];
+		REQUIRE(func() == (idx * 2));
+		REQUIRE(func() == func2());
+	}
+}
