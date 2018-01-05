8dfb6b2395447ff80d4503ee978ba8a39718ab83
Neil Forbes-Richardson <neilo@neilo.gd>
2018-01-05 13:32:06 +1100

    Autoformat will clean up clang-format generated .TMP files

--- scripts/autoformat.py
+++ scripts/autoformat.py
@@ -10,6 +10,14 @@ def findSource(root):
 				matches.append(os.path.join(root, filename))
 	return matches
 
+def findTempFiles(root):
+	matches = []
+	for root, dirnames, filenames in os.walk(root):
+		for ext in ["*.cpp~*.TMP", "*.h~*.TMP", "*.inl~*.TMP"]:
+			for filename in fnmatch.filter(filenames, ext):
+				matches.append(os.path.join(root, filename))
+	return matches
+
 
 def findClangFormat():
 	check = "C:\\Program Files\\LLVM\\bin\\clang-format.exe"
@@ -36,6 +44,18 @@ def autoformat():
 		for source in filteredSource:
 			print " -", source
 			subprocess.call([clangFormat, "-i", source])
+
+	foundTemp = findTempFiles("src")
+	foundTemp += findTempFiles("apps")
+
+	filteredTemp = []
+	# filter out folders with "_clang_fornat_ignore"
+	for temp in foundTemp:
+		ignoreFile = os.path.join(os.path.dirname(temp), "_clang_format_ignore")
+		if os.path.exists(ignoreFile) == False:
+			filteredTemp.append(temp)
+	for temp in filteredTemp:
+		os.remove(temp)
 
 	pass
 
