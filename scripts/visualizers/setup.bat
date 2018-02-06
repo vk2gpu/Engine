5646738c457a5a3213e5aa37f8a65c3dd75198a9
Neil Forbes-Richardson <neilo@neilo.gd>
2018-02-07 10:06:31 +1100

    Added natjmc and natstepfilter scripts, and setup.bat to copy them to the correct location

---
+++ scripts/visualizers/setup.bat
@@ -0,0 +1,3 @@
+xcopy Engine.natvis "%UserProfile%\Documents\Visual Studio 2017\Visualizers\Engine.natvis*" /Y
+xcopy Engine.natjmc "%UserProfile%\Documents\Visual Studio 2017\Visualizers\Engine.natjmc*" /Y
+xcopy Engine.natstepfilter "%UserProfile%\Documents\Visual Studio 2017\Visualizers\Engine.natstepfilter*" /Y
